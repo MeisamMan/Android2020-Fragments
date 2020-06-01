65b27860117847428662a2d874c8508141a38cee
Meisam <Meisam@meiCode.org>
2020-06-02 02:05:38 +0400

    The source code for the video called 'Fragments' in the Android App Development Master Class for 2020

---
+++ app/src/main/java/org/meicode/fragments/MainActivity.java
@@ -0,0 +1,19 @@
+package org.meicode.fragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, new FirstFragment());
+        transaction.commit();
+    }
+}
